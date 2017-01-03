9c6b1ca650554a78acf38ca007600735ff1656df
Piotr Celigowski <piotrcel2@gmail.com>
2017-01-04 00:23:27 +0100

    galleries arrows notworkingyet

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,50 @@
 document.addEventListener("DOMContentLoaded", function(){
     
     
+    var imgindex = 0;
+    var littleimgs = $('.gallerybox').children();
+    var numberofimgs = littleimgs.length;
+    
+    var images = $('.galleryimgs').children();
+    
+    var leftarrow = $('.fa-arrow-circle-left');
+    var rightarrow = $('.fa-arrow-circle-right');
+    
+    console.log(images[0]);
     
     
-    console.log('hej');
+    function setimage(){
+        
+        images.addClass('setdisnone');
+        
+        //images[imgindex].removeClass('setdisnone');
+        //dlaczego to nie działa to jeszcze nie rozgryzłem bo obiekt jest dobrze wyznaczony -_-
+    }
+    
+    
+    leftarrow.on('click', function(){
+        
+        if ( imgindex == 0){
+            imgindex = numberofimgs - 1;
+        } else {
+            imgindex -= 1;
+        }
+        
+        setimage();
+        
+    })
+    
+    rightarrow.on('click', function(){
+        
+        if ( imgindex == numberofimgs - 1){
+            imgindex = 0;
+        } else {
+            imgindex += 1;
+        }
+        
+        setimage();
+        
+    })
     
     
 })
